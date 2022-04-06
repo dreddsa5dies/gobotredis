4d394daccadd7645e9ee0401943c37cbb23b320e
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-04-06 14:10:14 +0300

    frame bot ok

--- bot/bot.go
+++ bot/bot.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"time"
 
 	tb "gopkg.in/tucnak/telebot.v2"
@@ -25,8 +26,34 @@ func Run() (*tb.Bot, error) {
 		return nil, err
 	}
 
-	bot.Handle(tb.OnText, func(m *tb.Message) {
-		bot.Send(m.Sender, "hello world")
+	bot.SetCommands([]tb.Command{
+		{Text: "/start", Description: "start the bot"},
+	})
+
+	bot.Handle("/start", func(m *tb.Message) {
+		bot.UnpinAll(m.Chat)
+		user := m.Sender
+		bot.Delete(m)
+
+		selectorLocale := &tb.ReplyMarkup{}
+		RuBtn := selectorLocale.Data("€ EUR", "eur_btn", "ru")
+		EnBtn := selectorLocale.Data("$ USD", "usd_btn", "en")
+		selectorLocale.Inline(selectorLocale.Row(RuBtn, EnBtn))
+		bot.Send(user, "Выбор валюты", selectorLocale)
+		bot.Handle(&RuBtn, func(c *tb.Callback) {
+			user := m.Sender
+			log.Printf("DEBUG: user %v request RUB/EUR", user.Username)
+			bot.Delete(c.Message)
+			bot.Send(user, "Цену пока не знаю")
+			bot.Respond(c)
+		})
+		bot.Handle(&EnBtn, func(c *tb.Callback) {
+			user := m.Sender
+			log.Printf("DEBUG: user %v request RUB/USD", user.Username)
+			bot.Delete(c.Message)
+			bot.Send(user, "Цену пока не знаю")
+			bot.Respond(c)
+		})
 	})
 
 	return bot, err
